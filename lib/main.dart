6778d830b70ffeb541dcbc55c0e83e5141b8efb0
johannes.madja <johannesmadja@gmail.com>
2026-06-01 22:10:24 +0100

    end first tuto

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,28 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: "Graven Learn App",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Graven Learning App")),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset("assets/images/imo.jpeg"),
+              const Text("AsynConf 2022", style: TextStyle(fontSize: 42, fontFamily: 'Poppins')),
+              Text(
+                "Salon de l'informatique pour l'année 2020",
+                style: TextStyle(fontSize: 30),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
